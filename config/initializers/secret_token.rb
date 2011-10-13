c5a0c8e2f6a700b9b0db5191c24e752035ac03ee
Upender Devulapally <upender@indigenius.com>
2011-10-13 13:06:13 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omniauth::Application.config.secret_token = '72c6d4488269d2a4482097746647c5dbda61507cd9acaa13ec51db2d44ee72900e489e3205fc4b3461d67bdae27d59c9b13057466916c3db84e74115363b36b5'
